576d237f304e9ddd03a67346b40ccc67be6657fb
Bjorn Bergdahl <bjorn.bergdahl@infor.com>
2019-08-27 20:58:51 +0200

    Database migrations done

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AspNetCoreTodo.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,17 @@ namespace AspNetCoreTodo.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
         {
+        }
+        public DbSet<TodoItem> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // ...
         }
     }
 }
