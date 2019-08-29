d8a6ed7d5172ffb44cefd293b3b9032d8057a199
Bjorn Bergdahl <bjorn.bergdahl@infor.com>
2019-08-29 11:02:41 +0200

    Added real database and functional connections

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Services;
-using AspNetCoreTodo.Services;
 using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Controllers
@@ -25,5 +24,35 @@ namespace AspNetCoreTodo.Controllers
             };
             return View(model);
         }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            var successful = await _todoItemService.AddItemAsync(newItem);
+            if (!successful)
+            {
+                return BadRequest("Could not add item.");
+            }
+            return RedirectToAction("Index");
+        }
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkDone(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+            }
+            var successful = await _todoItemService.MarkDoneAsync(id);
+            if (!successful)
+            {
+                return BadRequest("Could not mark item as done.");
+            }
+            return RedirectToAction("Index");
+        }
     }
+
 }
